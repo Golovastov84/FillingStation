9861dcd3f097b60d9bec7d6152ac6040bd7f9e59
Василий Головастов <vsgolovastov@gmail.com>
2021-11-30 19:35:17 +0400

    StudiesSkilbox_19353011

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,18 @@
+import java.math.RoundingMode;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Система расчёта стоимости топлива");
-        int fuelType = 92;
-        int amount = 420;
-        int maxAmount = 400;
-        double fuel92price = 60.2;
-        double fuel95price = 67.33;
+        int fuelType = 92; // Тип топлива
+        var amount = 12; // Заказываемое количество топлива.
+        // var тип переменоой определяется автоматисеки
+        boolean hasDiscount = amount > 10; // Наличие скидки, если выполняется условие
+        int maxAmount = 400; // Ограничение на единовременную покупку топлива
+        double fuel92price = 60.2; // Цена 1л 92 бензина
+        double fuel95price = 67.33; // Цена 1л 95 бензина
+        double discount = 0.1; // Величина скидки в долях
+        String wrongFuelTypeMessage = "Указан неверный тип топлива";
+        String fuelPriceMessage;
         double fuelPrice = 0;
         if(fuelType == 92) {
             fuelPrice = fuel92price;
@@ -14,7 +21,11 @@ public class Main {
             fuelPrice = fuel95price;
         }
         else {
-            System.out.println("Указан неверный тип топлива");
+            System.out.println(wrongFuelTypeMessage);
+        }
+        if(hasDiscount) {
+            fuelPrice = (1 - discount) * fuelPrice;
+            fuelPrice = (Math.ceil(fuelPrice * 100))/100;
         }
         if(amount < 1) {
             System.out.println("Указано слишком малое количество топлива");
@@ -26,8 +37,10 @@ public class Main {
                     + maxAmount + " л.");
             amount =  maxAmount;
         }
-        System.out.println("Цена выбранного топлива: " + fuelPrice + " руб.");
+        fuelPriceMessage = "Цена выбранного топлива: " + fuelPrice + " руб.";
+        System.out.println(fuelPriceMessage);
         double totalPrice = fuelPrice * amount;
+        totalPrice = (Math.ceil(totalPrice * 100))/100;
         System.out.println("Общая стоимость заправки: " + totalPrice + " руб.");
     }
 }
